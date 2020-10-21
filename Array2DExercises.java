da77814d288cbd4ef462f726fb4f6e5f4385b571
jpolacco <jpolacco@gmail.com>
2020-10-21 13:40:23 -0700

    Add files via upload

---
+++ Array2DExercises.java
@@ -0,0 +1,59 @@
+public class Array2DExercises 
+{
+    // returns the maximum value in the 2d parameter array 
+    public static int max(int[][] a) 
+    {
+        return -1;
+        
+    }
+
+    // returns the sum of the elements in row r of a. 
+    public static int rowSum(int[][] a, int r)
+    {
+        
+        return -1;
+    }
+
+    // returns the sum of the elements in column c of a (careful with rows of different lengths!).
+    public static int columnSum(int[][] a, int c)
+    {
+        return -1;
+    }
+
+    // calculates the row sum for every row and returns each of the values in an array. 
+    // index i of the return array contains the sum of elements in row i.
+    public static int[] allRowSums(int[][] a)
+    {
+        
+        return new int[] {};
+    }
+        
+    // checks if the array is row-magic (this means that every row has the same row sum).
+    public static boolean isRowMagic(int[][] a)
+    {
+        return false;
+    }
+
+    // checks if the array is column-magic (this means that every column has the same column sum).
+    public static boolean isColumnMagic(int[][] a)
+    {
+        return false;
+    }
+    
+    // checks if the array is square (i.e. every row has the same length as a itself).
+    public static boolean isSquare(int[][] a)
+    {
+        return false;
+    }
+
+    // checks if the array is a magic square. This means that it must be square, 
+    // and that all row sums, all column sums, and the two diagonal-sums must all be equal.
+    public static boolean isMagic(int[][] a)
+    {
+        return false;
+        
+    }
+
+
+ 
+}
