305565839bb2aa4eeec536ff06644c265d10b8cb
jpolacco <jpolacco@gmail.com>
2022-10-04 13:15:23 -0700

    Add files via upload

---
+++ Array2DExercises.java
@@ -0,0 +1,92 @@
+//TODO: Complete all of the static methods below 
+//
+public class Array2DExercises 
+{
+    /**
+     * @param arr a 2D array of ints
+     * @return the maximum value in the 2D parameter array 
+     */ 
+    public static int max(int[][] arr) 
+    {
+        return -1;  // delete this line of code
+        
+    }
+
+    /**
+     * @return the sum of the elements in row r of arr.
+     * @param arr a 2D array of ints
+     * @param r the row index
+     */  
+    public static int rowSum(int[][] arr, int r)
+    {
+        
+        return -1;   // delete this line of code
+    }
+
+    /**
+     *  @return the sum of the elements in column c of arr (careful with rows of different lengths!)
+     *  @param arr a 2D array of ints
+     *  @param c the column index
+     */ 
+    public static int columnSum(int[][] arr, int c)
+    {
+        return -1;   // delete this line of code
+    }
+
+    /**
+     * @return an array of the row sums. Note: index i of the return array contains the sum
+     * of elements in row i.
+     * @param arr a 2D array of ints
+     */ 
+    public static int[] allRowSums(int[][] arr)
+    {
+        return new int[] {};    // delete this line of code
+    }
+        
+    /**
+     *     checks if an array is row-magic (this means that every row has the same row sum).
+     *     @return true if arr is row-magic; false otherwise
+     *     @param arr a 2D array of ints
+     */
+    public static boolean isRowMagic(int[][] arr)
+    {
+        return false;   // delete this line of code
+    }
+
+    /**
+     * checks if a rectangular array is column-magic (this means that every column has the same column sum).
+     * pre-condition: arr is rectangular 2-D array
+     * @return true if arr is column-magic; false otherwise
+     * @param arr a rectangular 2D array of ints
+     * 
+     */
+    public static boolean isColumnMagic(int[][] arr)
+    {
+        return false;   // delete this line of code
+    }
+    
+    /**
+     * checks if the array is square (i.e. every row has the same length as a itself).
+     * @param arr a 2D array of ints
+     * @return true if arr is square; false otherwise
+     */
+    public static boolean isSquare(int[][] arr)
+    {
+        return false;   // delete this line of code
+    }
+
+    /**
+     * Checks if the array is a magic square. This means that it must be square, 
+     * and that all row sums, all column sums, and the two diagonal-sums must all be equal.
+     * @param arr a 2D array of ints
+     * @return true if arr is a magic square; false otherwise
+     */
+    public static boolean isMagic(int[][] arr)
+    {
+        return false;   // delete this line of code
+        
+    }
+
+
+ 
+}
